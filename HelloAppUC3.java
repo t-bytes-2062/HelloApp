3bae33bb856d369114bc7aef7d7d8755d76b0b9b
t-bytes-2062 <tj2062@srmist.edu.in>
2026-03-17 08:38:17 +0530

    Add files via upload

---
+++ HelloAppUC3.java
@@ -0,0 +1,9 @@
+public class HelloAppUC3 {
+public static void main(String[] args) {
+if(args.length == 0){
+System.out.println("Hello Guest");
+} else {
+System.out.println("Hello " + args[0]);
+}
+}
+}
